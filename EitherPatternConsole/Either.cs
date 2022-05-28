6c1e1592115e31f187828fce91735667b776b912
Renan Rolo <renanrolo@hotmail.com>
2022-05-28 10:31:15 -0400

    refactoring

---
+++ EitherPatternConsole/Either.cs
@@ -0,0 +1,25 @@
+﻿namespace EitherPatternConsole
+{
+    public class Either<TLeft, TRight>
+    {
+        public TLeft? Left { get; private set; }
+        public TRight? Right { get; private set; }
+
+        public bool IsRight { get; private set; }
+
+        public Either(TLeft left)
+        {
+            IsRight = false;
+            Left = left;
+        }
+
+        public Either(TRight right)
+        {
+            IsRight = true;
+            Right = right;
+        }
+
+        public static implicit operator Either<TLeft, TRight>(TLeft left) => new(left);
+        public static implicit operator Either<TLeft, TRight>(TRight right) => new(right);
+    }
+}
--- EitherPatternConsole/Program.cs
+++ EitherPatternConsole/Program.cs
@@ -1,14 +1,16 @@
-﻿#nullable enable
-
-using System;
+﻿using System;
+using System.Linq;
 
 namespace EitherPatternConsole
 {
     class Program
     {
+        private static readonly string[] _existingNames = new string[] { "Grid", "Skuld" };
+        private static readonly string[] _namesToAdd = new string[] { "Roald", "Grid", "Reidar", "Skuld", "Brandr" };
+
         static void Main()
         {
-            foreach (var name in new string[] { "Roald", "Grid", "Reidar", "Skuld", "Brandr" })
+            foreach (var name in _namesToAdd)
             {
                 TryCreatePerson(name);
             }
@@ -22,55 +24,32 @@ namespace EitherPatternConsole
 
             var result = CreatePerson(personToCreate);
 
+            var consolePrefix = name.PadRight(10, ' ');
+
             if (result.IsRight)
             {
-                Console.WriteLine($"Person '{result.Right.Name}' created successfully");
+                
+                Console.WriteLine($"{consolePrefix}>  Created successfully");
                 return;
             }
 
-            Console.WriteLine($"Person was not created: {result.Left.Error}");
+            Console.WriteLine($"{consolePrefix}>  Error when creating person, reason: {result.Left.Error}");
         }
 
         static Either<NameAlreadyTaken, Person> CreatePerson(Person person)
         {
             /* Here we can se the magic of the inplicity operator:
-             * We dont need to do 'return new Either<NameAlreadyTaken, Person>(new NameAlreadyTaken())' on line 47,
-             * the inplicity operator on line 73/74 do that for us. Its the same as doing:
+             * We dont need to do 'return new Either<NameAlreadyTaken, Person>(new NameAlreadyTaken())' on line 46,
+             * the inplicity operator defined on the Either class do that for us. Its the same as doing:
              * Either<NameAlreadyTaken, Person> either = new NameAlreadyTaken(); //TLeft
              * Either<NameAlreadyTaken, Person> either = new Person(); //TRight
              */
-            var random = new Random();
-            var result = random.Next(0, 4);
-
-            if (result <= 1)
+            if (_existingNames.Contains(person.Name))
             {
                 return new NameAlreadyTaken();
             }
 
             return person;
         }
-    }
-
-    public class Either<TLeft, TRight>
-    {
-        public TLeft? Left { get; private set; }
-        public TRight? Right { get; private set; }
-
-        public bool IsRight { get; private set; }
-
-        public Either(TLeft left)
-        {
-            IsRight = false;
-            Left = left;
-        }
-
-        public Either(TRight right)
-        {
-            IsRight = true;
-            Right = right;
-        }
-
-        public static implicit operator Either<TLeft, TRight>(TLeft left) => new(left);
-        public static implicit operator Either<TLeft, TRight>(TRight right) => new(right);
     }
 }
